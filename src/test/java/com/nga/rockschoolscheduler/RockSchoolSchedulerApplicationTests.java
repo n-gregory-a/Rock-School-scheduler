e4abc5af0e256e56d8d9f2bff2759e52c9bc86b6
Grigory <ovonely@gmail.com>
2020-03-09 19:20:27 +0700

    creating project

---
+++ src/test/java/com/nga/rockschoolscheduler/RockSchoolSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nga.rockschoolscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RockSchoolSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
